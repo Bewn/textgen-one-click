3be01082af5c1789b719c11adf0afe1895af8cec
Ben Rosebery <benrosebery@gmail.com>
2023-05-09 23:51:34 -0400

    loading good

--- run.sh
+++ run.sh
@@ -11,28 +11,30 @@ _add_env_var () {
 create_textgenrc () {
 	if [ ! -f $HOME/.textgenrc ]
 		then touch $HOME/.textgenrc
-		$(echo "# bashrc style environtment and function space") >> $HOME/.textgenrc
+		$(echo "# bashrc style environtment and function space" >> $HOME/.textgenrc)
 		_add_env_var;
 	fi
 }
 
 init_env () {
-    true
+    source $HOME/.textgenrc
+    echo ".textgenrc loaded"
 }
 
 run () {
-    if [ -f $HOME/.textgenrc ];
-        then source $HOME/.textgenrc
-        echo ".textgenrc loaded";
-        else create_textgenrc;
-    fi
-
     if [ -z $TEXTGEN_DIR ];
-        then echo "environment variable TEXTGEN_DIR not set"
-        exit 0;
+        then echo "environment variable TEXTGEN_DIR not set, setting now, please run again.";
     fi
-}
 
+     if [ -f $HOME/.textgenrc ];
+        then init_env;
+        else create_textgenrc && init_env;
+    fi
+    
+    # load in header now that TEXTGEN_DIR is ensured
+    source $TEXTGEN_DIR/init.header.sh
+    echo "init header loaded"
+} 
 run
 #micromamba --version
 
